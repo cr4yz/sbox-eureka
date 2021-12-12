14fd5d4999b5b5adc67824ca2a4b9d7dd2c7fddd
Crayz <jacob.ee92@gmail.com>
2021-12-11 20:54:27 -0600

    Update EurekaApi.cs

--- Code/API/EurekaApi.cs
+++ Code/API/EurekaApi.cs
@@ -8,11 +8,11 @@ namespace Eureka.Api;
 internal static partial class EurekaApi
 {
 
-    public const string ApiEndpoint = "https://localhost:7201/api";
+    public const string Endpoint = "https://localhost:7201/api";
 
     public static async Task<T> Fetch<T>(string controller)
     {
-        var http = new Http(new System.Uri($"{ApiEndpoint}/{controller}"));
+        var http = new Http(new System.Uri($"{Endpoint}/{controller}"));
         var json = await http.GetStringAsync();
 
         return JsonSerializer.Deserialize<T>(json);
